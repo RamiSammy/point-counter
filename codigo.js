f7c57a89ec4c82b4e2272aa8898283bc9d55b9f6
ramirolewin <ramirolewin@hotmail.com>
2021-03-10 03:07:34 -0600

    Lo mismo pero con comentarios
    Puse comentarios para entender mejor que estoy haciendo en cada linea

--- codigo.js
+++ codigo.js
@@ -29,13 +29,13 @@ let tieBreakJugador2 = 0;
 
 //Cuando tocan la foto del jugador 1 aumenta el contador 
 puntoJugador1.onclick = function(){
-    const puntaje = document.querySelector(".resultado");
-    const actualizar = primera2Letras(puntaje.textContent);
-    const igual = ultimas2Letras(puntaje.textContent);
+    const puntaje = document.querySelector(".resultado"); // Accede a lo que esta escrito en el cuadro de resultado y lo guarda en la variable puntaje
+    const actualizar = primera2Letras(puntaje.textContent); // Hice una funcion aparte para que me diga las primeras 2 letras de un texto y llamo a puntaje
+    const igual = ultimas2Letras(puntaje.textContent); // Lo mismo pero con las ultimas 2 letras
 if(valorTieBreak == false){
-    switch (actualizar) {
+    switch (actualizar) {     //Separo en 2, 1 cuando estamos en tieBreak y otra en la situacion normal
         case "0 ":
-            puntaje.textContent = `15 - ${igual}`;
+            puntaje.textContent = `15 - ${igual}`; //Cambia el resultado
             break;
         case "15":
             puntaje.textContent = `30 - ${igual}`;
@@ -44,15 +44,15 @@ if(valorTieBreak == false){
             puntaje.textContent = `40 - ${igual}`;
             break;
         case "40" :
-                if(igual != 40 && igual != "Ad"){
+                if(igual != 40 && igual != "Ad"){ //Caso en donde gana sin llegar a Ad
                     puntaje.textContent = `0 - 0`;
-                    cambioContador(1);
+                    cambioContador(1); //Funcion creada para sumar el game o el set dependiendo el caso particular
                      break;
-                } else if (igual == "40"){
-                    puntaje.textContent = `Ad - ${igual}`;
+                } else if (igual == "40"){ // Caso en donde van 40-40, entonces tendria que cambiarse a Ad-40
+                    puntaje.textContent = `Ad - ${igual}`; 
                      break;
                 } else {
-                    puntaje.textContent = `40 - 40`;
+                    puntaje.textContent = `40 - 40`;  //Y por ultimo queda en el caso en que iban 30-40 y llega a 40-40
                      break;
                 }   
         case "Ad" :
@@ -60,14 +60,14 @@ if(valorTieBreak == false){
             cambioContador(1);
             break;
     }
-}else{
-    tieBreakJugador1++;
-    if(tieBreakJugador1 > 6 && (tieBreakJugador1-tieBreakJugador2 > 1)){
+}else{ // Aca es donde empieza el tiebreak
+    tieBreakJugador1++; // Dentro de la suma de puntos del jugador 1 se le suma 1
+    if(tieBreakJugador1 > 6 && (tieBreakJugador1-tieBreakJugador2 > 1)){ // Verificacion de si se tiene que seguir el tieBreak
         puntaje.textContent = `0 - 0`;
         cambioContador(1);
      }else{
-        puntaje.textContent = `${tieBreakJugador1} - ${tieBreakJugador2}`;
-        document.querySelector(".contador").rows[1].cells[setActual].textContent = `6 (${tieBreakJugador1})`;
+        puntaje.textContent = `${tieBreakJugador1} - ${tieBreakJugador2}`; // cambia el resultado en el puntaje
+        document.querySelector(".contador").rows[1].cells[setActual].textContent = `6 (${tieBreakJugador1})`; //Cambia el resultado en el contador
         
      }
 }
@@ -75,7 +75,7 @@ if(valorTieBreak == false){
 };
 
 //Cuando tocan la foto del jugador 2 aumenta el contador 
-
+//El comportamiento es muy similar al anterior, fijarse 
 const puntoJugador2 = document.querySelector(".foto-jugador2");
 puntoJugador2.onclick = function(){
     const puntaje = document.querySelector(".resultado");
@@ -125,42 +125,42 @@ puntoJugador2.onclick = function(){
 
 
 // Una vez ya sabido quien gano el juego se va a aumentar dependiendo el caso 
+//Funcion de cambioContador
 
-
-function cambioContador (numeroJugador) {
-    if(numeroJugador == 1){
+function cambioContador (numeroJugador) {//numeroJugador es 1 o 2 , el 1 representa al jugador1 y el 2 para el jugador2
+    if(numeroJugador == 1){ // Verifico quien gano el game y le aumento 1
         gameJugador1 ++;
     }else{
         gameJugador2 ++;
     }
     
-    const resultado   = document.querySelector(".contador").rows[numeroJugador].cells[setActual];
+    const resultado   = document.querySelector(".contador").rows[numeroJugador].cells[setActual]; //Accede al dato que esta ubicado en el contador
 
-    if(gameJugador1 == 6 && gameJugador2 == 6){
+    if(gameJugador1 == 6 && gameJugador2 == 6){ // Separo en 2, tiebreak y normal, aca acceden si estan en tiebreak, el valorTieBreak es true y se cambia el contador
         valorTieBreak = true;
         document.querySelector(".contador").rows[1].cells[setActual].textContent = `6 (${tieBreakJugador1})`;
         document.querySelector(".contador").rows[2].cells[setActual].textContent = `6 (${tieBreakJugador2})`;
         
 } else {
-    if(numeroJugador == 1){
+    if(numeroJugador == 1){ // Aca simplemente se remplaza el numero del contador por el nuevo 
         resultado.textContent = gameJugador1;
     }else{
         resultado.textContent = gameJugador2;
     }
 }
 
-    if ((gameJugador1 == 6 && gameJugador2 < 5) | (gameJugador2 == 6 && gameJugador1 < 5) ){
+    if ((gameJugador1 == 6 && gameJugador2 < 5) | (gameJugador2 == 6 && gameJugador1 < 5) ){ //Caso en que ganan sin llegar a diferencia/tiebreak
         setActual ++;
         gameJugador1 = 0;
         gameJugador2 = 0;
     }
 
-    if ((gameJugador1 == 7 && gameJugador2 == 5) | (gameJugador2 == 7 && gameJugador1 == 5)){
+    if ((gameJugador1 == 7 && gameJugador2 == 5) | (gameJugador2 == 7 && gameJugador1 == 5)){//Caso de diferencia
         setActual ++;
         gameJugador1 = 0;
         gameJugador2 = 0;
     }
-    if((gameJugador1 == 7 && gameJugador2 == 6) | (gameJugador2 == 7 && gameJugador1 == 6)){
+    if((gameJugador1 == 7 && gameJugador2 == 6) | (gameJugador2 == 7 && gameJugador1 == 6)){//Caso de tiebreak
         setActual++;
         gameJugador1 = 0;
         gameJugador2 = 0;
