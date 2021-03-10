9abe5a69a4a983124513f35d154d2c42d7649fb4
ramirolewin <ramirolewin@hotmail.com>
2021-03-09 18:54:12 -0600

    Mejora
    Ahora funciona correctamente el puntaje, hice un pequeño avance con el contador pero hay bug todavia

--- codigo.js
+++ codigo.js
@@ -1,14 +1,12 @@
 
-// const nuevo_puntaje = "15 - 0";
-// const puntaje = document.querySelector(".resultado");
-// puntaje.textContent = nuevo_puntaje;
+
 
 
 
 const puntoJugador1 = document.querySelector(".foto-jugador1");
 
 let contador = 0;
-// console.log(puntoJugador1.innerHTML);
+
 
 function primera2Letras (frase) {
     return frase[0]+frase[1];
@@ -27,19 +25,10 @@ puntoJugador1.onclick = function(){
     const puntaje = document.querySelector(".resultado");
     const actualizar = primera2Letras(puntaje.textContent);
     const igual = ultimas2Letras(puntaje.textContent);
-    // console.log(actualizar);
-    // console.log(actualizar == "0 " );
-    // console.log("15 - " + igual);
-    // console.log(typeof actualizar);
-    // console.log(typeof 12 );
-    // console.log(ultimas2Letras("30"));
-    // console.log(ultimas2Letras("30 - 15"));
-    // console.log(ultimas2Letras("0 - 0"));
-    // console.log(ultimas2Letras("0 - 40"));
+
     switch (actualizar) {
         case "0 ":
             puntaje.textContent = `15 - ${igual}`;
-            console.log("llegue");
             break;
         case "15":
             puntaje.textContent = `30 - ${igual}`;
@@ -47,8 +36,21 @@ puntoJugador1.onclick = function(){
         case "30":
             puntaje.textContent = `40 - ${igual}`;
             break;
-        case "40":
-            puntaje.textContent = `0 - ${igual}`;
+        case "40" :
+                if(igual != 40 && igual != "Ad"){
+                    puntaje.textContent = `0 - 0`;
+                    cambioContador();
+                     break;
+                } else if (igual == "40"){
+                    puntaje.textContent = `Ad - ${igual}`;
+                     break;
+                } else {
+                    puntaje.textContent = `40 - 40`;
+                     break;
+                }   
+        case "Ad" :
+            puntaje.textContent = `0 - 0`;
+            cambioContador();
             break;
     }
 };
@@ -59,13 +61,10 @@ puntoJugador2.onclick = function(){
     const puntaje = document.querySelector(".resultado");
     const actualizar = ultimas2Letras(puntaje.textContent);
     const igual = primera2Letras(puntaje.textContent);
-    console.log("llegue");
-    console.log(actualizar == " 0");
   
     switch (actualizar) {
         case " 0":
             puntaje.textContent = `${igual} - 15`;
-            console.log("llegue");
             break;
         case "15":
             puntaje.textContent = `${igual} - 30`;
@@ -73,8 +72,37 @@ puntoJugador2.onclick = function(){
         case "30":
             puntaje.textContent = `${igual} - 40`;
             break;
-        case "40":
-            puntaje.textContent = `${igual} - 0`;
+        case "40" :
+            if(igual != 40 && igual != "Ad"){
+                 puntaje.textContent = `0 - 0`;
+                    break;
+            } else if (igual == "40"){
+                 puntaje.textContent = `${igual} - Ad`;
+                 break;
+            } else {
+                puntaje.textContent = `40 - 40`;
+                    break;
+            }   
+        case "Ad" :
+            puntaje.textContent = `0 - 0`;
             break;
     }
 };
+
+
+// Practicando accediendo a la tabla
+let setActual = 1;
+let gameJugador1 = 0;
+let gameJugador2 = 0;
+
+function cambioContador () {
+    gameJugador1 ++;
+    const resultado   = document.querySelector(".contador").rows[1].cells[setActual];
+    resultado.textContent = gameJugador1;
+    if (gameJugador1 == 6){
+        setActual ++;
+        gameJugador1 = 0;
+    }
+}
+
+
